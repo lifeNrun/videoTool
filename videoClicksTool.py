3b87740c86cf0608afd15bcb9fe22792c33511ad
lifeNrun <2317952229@qq.com>
2018-08-05 06:42:08 +0800

    change function clickVideoPyvirtualdisplayMoreWindow

--- videoClicksTool.py
+++ videoClicksTool.py
@@ -4,39 +4,48 @@ from selenium.webdriver.common.action_chains import ActionChains
 from pyvirtualdisplay import Display
 import sys
 import random
+import threading
+
 userAgentArray = []
 proxyIps = []
 
-def clickVideoPyvirtualdisplayMoreWindow(videoUrl, intSleepTime):
+def runPyvirtualdisplayBrowser(chromeOptions, intSleepTime):
   display = Display(visible=0, size=(800, 600))
   display.start()
-  chromeOptions = webdriver.ChromeOptions()
-  num = random.randint(2, 3)
-  i = 0
-  chromeOptions.binary_location='/usr/bin/chromium-browser'
-  # 比如模拟 Android Chrome
-  while i < num:
-    userAgent = 'user-agent='+ getRandomUserAgent()
-    print(userAgent)
-    chromeOptions.add_argument(userAgent)
-    chromeOptions.add_argument('--ignore-certificate-errors')
-    chromeOptions.add_argument('--ignore-ssl-errors')
-    chromeOptions.add_argument('--no-sandbox')
-    browser = webdriver.Chrome(chrome_options=chromeOptions)
-    browser.set_window_size(800,600)
-    # 访问
-    browser.get(videoUrl)
-    #定位到要右击的元素
-    youtubeVideo = browser.find_element_by_id("player")
-    #对定位到的元素执行鼠标右键操作
-    ActionChains(browser).click(youtubeVideo).perform()
-    i = i + 1
-
-  timeInt = random.randint(intSleepTime - 10, intSleepTime)
+  userAgent = 'user-agent='+ getRandomUserAgent()
+  print(userAgent)
+  chromeOptions.add_argument(userAgent)
+  chromeOptions.add_argument('--ignore-certificate-errors')
+  chromeOptions.add_argument('--ignore-ssl-errors')
+  chromeOptions.add_argument('--no-sandbox')
+  browser = webdriver.Chrome(chrome_options=chromeOptions)
+  browser.set_window_size(800,600)
+  # 访问
+  browser.get(videoUrl)
+  #定位到要右击的元素
+  youtubeVideo = browser.find_element_by_id("player")
+  #对定位到的元素执行鼠标右键操作
+  ActionChains(browser).click(youtubeVideo).perform()
+  timeInt = random.randint(intSleepTime - 5, intSleepTime)
   time.sleep(timeInt)
   display.stop()
   # 退出，清除浏览器缓存
   browser.quit()
+
+def clickVideoPyvirtualdisplayMoreWindow(videoUrl, intSleepTime):
+  chromeOptions = webdriver.ChromeOptions()
+  num = random.randint(3, 4)
+  i = 0
+  chromeOptions.binary_location='/usr/bin/chromium-browser'
+  threads = []
+  while i < num:
+    t = threading.Thread(target=runPyvirtualdisplayBrowser, args=(chromeOptions, intSleepTime))
+    threads.append(t)
+    i = i + 1
+  for s in threads: # 开启多线程爬取
+    s.start()
+  for e in threads: # 等待所有线程结束
+    e.join()
 
 # 读取文档
 def read(path):
