616a2cd4b73d9da7e06089a49dae6654d3e15467
lifeNrun <2317952229@qq.com>
2018-08-07 07:22:18 +0800

    add function searchAndClickVideo

--- videoClicksTool.py
+++ videoClicksTool.py
@@ -8,8 +8,36 @@ import threading
 
 userAgentArray = []
 proxyIps = []
+def searchAndClickVideo(chromeOptions, intSleepTime, url):
+  display = Display(visible=0, size=(800, 600))
+  display.start()
+  userAgent = 'user-agent='+ getRandomUserAgent()
+  print(userAgent)
+  chromeOptions.add_argument(userAgent)
+  chromeOptions.add_argument('--ignore-certificate-errors')
+  chromeOptions.add_argument('--ignore-ssl-errors')
+  chromeOptions.add_argument('--no-sandbox')
+  browser = webdriver.Chrome(chrome_options=chromeOptions)
+  browser.set_window_size(800,600)
+  # 查看本机ip，查看代理是否起作用
+  browser.get(url)
+  #定位到要单击的元素
+  try:
+    youtubeVideo = browser.find_element_by_css_selector('ytm-compact-video-renderer.item:first-child')
+  except Exception as error:
+    print(error)
+    browser.get(url)
+    # 对特别版本的android
+    youtubeVideo = browser.find_element_by_css_selector("div.cib[data-index='0']>div>div>a")
 
-def runPyvirtualdisplayBrowser(chromeOptions, intSleepTime):
+  ActionChains(browser).click(youtubeVideo).perform()
+  timeInt = random.randint(intSleepTime - 5, intSleepTime)
+  time.sleep(timeInt)
+  display.stop()
+  # 退出，清除浏览器缓存
+  browser.quit()
+
+def runPyvirtualdisplayBrowser(chromeOptions, intSleepTime, videoUrl):
   display = Display(visible=0, size=(800, 600))
   display.start()
   userAgent = 'user-agent='+ getRandomUserAgent()
@@ -39,7 +67,7 @@ def clickVideoPyvirtualdisplayMoreWindow(videoUrl, intSleepTime):
   chromeOptions.binary_location='/usr/bin/chromium-browser'
   threads = []
   while i < num:
-    t = threading.Thread(target=runPyvirtualdisplayBrowser, args=(chromeOptions, intSleepTime))
+    t = threading.Thread(target=searchAndClickVideo, args=(chromeOptions, intSleepTime, videoUrl))
     threads.append(t)
     i = i + 1
   for s in threads: # 开启多线程爬取
